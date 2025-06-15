09257fba8a68dfa92598174555397e3a2469acdb
daniel-kav <dankavatha01@gmail.com>
2025-06-15 20:58:15 +0300

    refactor(app): restructure app module and add global configurations
    - Remove default app controller and service in favor of PrSummaryModule
    - Add CORS support and global validation pipe in main.ts
    - Delete unused app controller spec file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrSummaryModule } from './pr-summary/pr-summary.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrSummaryModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
